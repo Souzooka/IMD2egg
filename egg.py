42c3e771fbf3a5f3a076bb77aebb01a8355f6097
Dakota Smith <dakotachasesmith@gmail.com>
2026-06-18 21:06:08 -0400

    egg: Refactor egg writing to process IMD primitive objects sequentially

--- egg.py
+++ egg.py
@@ -3,13 +3,15 @@ from __future__ import annotations
 import copy
 from typing import TextIO, cast
 
-from imd import IMD, IMDPrimGroup, IMDPrimTexture, IMDPrimVertexPool, IMDPrimVertexColor
-from util import Vec4
+from imd import IMD, IMDPrimGroup, IMDPrimTexture, IMDPrimTransformState, IMDPrimVertexPool, IMDPrimVertexColor, IMDPrim0x2, IMDPrim0x13
+from util import Color4, Vec4
 
 # Various constants yadayada
 COORDINATE_SYSTEM = "Z-up"
 INDENT_AMOUNT = 2
 
+def _write_with_indent(output_file: TextIO, msg: str, indent: int):
+    output_file.write(" " * indent + msg)
 
 class Egg:
 
@@ -37,82 +39,23 @@ class Egg:
         texture_id = texture.texture_id
         texture_path = f"TEX/{texture_id}.png"
 
-        Egg._write_with_indent(output_file, f"<Texture> {texture_id} {{\n", indent)
+        _write_with_indent(output_file, f"<Texture> {texture_id} {{\n", indent)
         indent += INDENT_AMOUNT
 
-        Egg._write_with_indent(output_file, f"\"{texture_path}\"\n", indent)
+        _write_with_indent(output_file, f"\"{texture_path}\"\n", indent)
 
         # TODO: Write any texture scalars here
 
         indent -= INDENT_AMOUNT
-        Egg._write_with_indent(output_file, "}", indent)
+        _write_with_indent(output_file, "}", indent)
         output_file.write("\n\n")
 
     @staticmethod
     def _write_group(output_file: TextIO, group: IMDPrimGroup, group_name: str, indent: int = 0):
-        # Let's assume there's a texture in here...
-        texture_id = 0
-        for prim in group.get_prims():
-            if isinstance(prim, IMDPrimTexture):
-                texture_id = prim.texture_id
-                break
-
-        Egg._write_with_indent(output_file, f"<Group> {group_name} {{\n", indent)
-        indent += INDENT_AMOUNT
-        
-        # I guess we'll just assume there's a vertex pool and write it for now
-        vp_id = 0
-        for i, prim in enumerate((prims := group.get_prims())):
-            if isinstance(prim, IMDPrimVertexPool):
-                # FIXME jank -- have to refactor this when IMD is better understood
-                # What the model is probably doing is reading primitives in order
-                # and just configuring various settings, before it gets to a particular vertex
-                # pool -- in that sense what we should really be doing in this function is
-                # just interpreting the primitives in order.
-                vertex_color_info = None
-                if i != 0 and isinstance(prims[i-1], IMDPrimVertexColor):
-                    vertex_color_info = cast("IMDPrimVertexColor", prims[i-1])
-                Egg._write_vertex_pool(output_file, prim, f"{group_name}.verts{vp_id}", texture_id, vertex_color_info, indent)
-                vp_id += 1
-
-        indent -= INDENT_AMOUNT
-        Egg._write_with_indent(output_file, "}\n", indent)
+        EggGroup(group, group_name).write(output_file, indent)
 
     @staticmethod
-    def _write_vertex_pool(output_file: TextIO, vertex_pool: IMDPrimVertexPool, pool_name: str, texture_id: int, vertex_color_info: IMDPrimVertexColor | None, indent: int = 0):
-        Egg._write_with_indent(output_file, f"<VertexPool> {pool_name} {{\n", indent)
-        indent += INDENT_AMOUNT
-
-        for i, vertex in enumerate(vertex_pool.vertices):
-            vec = Egg._convert_imd_coordinates(vertex.position)
-            Egg._write_with_indent(output_file, f"<Vertex> {i} {{ {vec.x} {vec.y} {vec.z}\n", indent)
-            indent += INDENT_AMOUNT
-            Egg._write_with_indent(output_file, f"<UV> {{ {vertex.u} {vertex.v} }}\n", indent)
-            indent -= INDENT_AMOUNT
-            Egg._write_with_indent(output_file, "}\n", indent)
-        indent -= INDENT_AMOUNT
-        Egg._write_with_indent(output_file, "}\n", indent)
-
-        # TODO: How do these vertices connect together...?
-        # NOTE: These are probably always triangle strips...
-        r, g, b, a = 1.0, 1.0, 1.0, 1.0
-        if vertex_color_info is not None:
-            color = vertex_color_info.color_scale
-            r, g, b, a = color.r, color.g, color.b, color.a
-        for i in range(0, len(vertex_pool.vertices)-2):
-            Egg._write_with_indent(output_file, f"<Polygon> {{\n", indent)
-            indent += INDENT_AMOUNT
-            if texture_id != 0:
-                Egg._write_with_indent(output_file, f"<TRef> {{ {texture_id} }}\n", indent)
-            Egg._write_with_indent(output_file, f"<RGBA> {{ {r} {g} {b} {a} }}\n", indent)
-            Egg._write_with_indent(output_file, f"<BFace> {{ 0 }}\n", indent)
-            Egg._write_with_indent(output_file, f"<VertexRef> {{ {i+0} {i+1} {i+2} <Ref> {{ {pool_name} }} }}\n", indent)
-            indent -= INDENT_AMOUNT
-            Egg._write_with_indent(output_file, "}\n", indent)
-        indent -= INDENT_AMOUNT
-
-    @staticmethod
-    def _convert_imd_coordinates(vec: Vec4):
+    def convert_imd_coordinates(vec: Vec4):
         # NOTE: IMD's Up is Y-down
         vec = copy.copy(vec)
         match COORDINATE_SYSTEM:
@@ -121,7 +64,118 @@ class Egg:
             case _:
                 raise RuntimeError("Unsupported .egg coordinate system")
         return vec
-            
-    @staticmethod
-    def _write_with_indent(output_file: TextIO, msg: str, indent: int):
-        output_file.write(" " * indent + msg)
+
+class EggGroup:
+    # user properties
+    name: str
+    # internal
+    prim_group: IMDPrimGroup
+    indent: int
+    output_file: TextIO | None
+    __num_vertex_pools: int # bit of a hack just to ensure unique vertex pool names for now
+    # model properties
+    vertex_rgba: Color4
+    texture_id: int
+
+    # TODO: Reordering egg groups later to nest them before writing them
+    parent_group_id: int
+    children: list[EggGroup]
+
+    def __init__(self, prim_group: IMDPrimGroup, name: str) -> None:
+        self.name = name
+        self.prim_group = prim_group
+        self.indent = 0
+        self.output_file = None
+        self.__num_vertex_pools = 0
+        self.vertex_rgba = Color4(1.0, 1.0, 1.0, 1.0)
+        self.texture_id = -1
+        self.parent_group_id = -1
+        self.children = []
+
+    def write(self, output_file: TextIO, indent: int = 0) -> None:
+        self.indent = indent
+        self.output_file = output_file
+
+        _write_with_indent(output_file, f"<Group> {self.name} {{\n", self.indent)
+        self.indent += INDENT_AMOUNT
+
+        for prim in self.prim_group.get_prims():
+            match prim.type:
+                case 0x01:
+                    assert isinstance(prim, IMDPrimGroup)
+                    self.__proc_prim_group(prim)
+                case 0x02:
+                    assert isinstance(prim, IMDPrim0x2)
+                    self.__proc_prim_02(prim)
+                case 0x10:
+                    assert isinstance(prim, IMDPrimTransformState)
+                    self.__proc_prim_transform_state(prim)
+                case 0x13:
+                    assert isinstance(prim, IMDPrim0x13)
+                    self.__proc_prim_13(prim)
+                case 0x20:
+                    assert isinstance(prim, IMDPrimVertexColor)
+                    self.__proc_prim_vertex_color(prim)
+                case 0x21:
+                    assert isinstance(prim, IMDPrimTexture)
+                    self.__proc_prim_texture(prim)
+                case 0x48:
+                    assert isinstance(prim, IMDPrimVertexPool)
+                    self.__proc_prim_vertex_pool(prim)
+                case _:
+                    print(f"EggGroup: Unknown prim type {hex(prim.type)}")
+
+        self.indent -= INDENT_AMOUNT
+        _write_with_indent(output_file, "}\n", indent)
+
+        self.output_file = None
+
+    def __proc_prim_group(self, prim_group: IMDPrimGroup):
+        raise RuntimeError("Nested PrimGroup unsupported")
+
+    def __proc_prim_02(self, prim: IMDPrim0x2):
+        pass
+
+    def __proc_prim_transform_state(self, prim: IMDPrimTransformState):
+        pass
+    
+    def __proc_prim_13(self, prim: IMDPrim0x13):
+        pass
+
+    def __proc_prim_vertex_color(self, prim: IMDPrimVertexColor):
+        self.vertex_rgba = prim.color_scale
+
+    def __proc_prim_texture(self, prim: IMDPrimTexture):
+        self.texture_id = prim.texture_id
+
+    def __proc_prim_vertex_pool(self, prim: IMDPrimVertexPool):
+        assert self.output_file is not None
+        pool_name = f"{self.name}_{self.__num_vertex_pools}.verts"
+        _write_with_indent(self.output_file, f"<VertexPool> {pool_name} {{\n", self.indent)
+        self.indent += INDENT_AMOUNT
+
+        for i, vertex in enumerate(prim.vertices):
+            vec = Egg.convert_imd_coordinates(vertex.position)
+            _write_with_indent(self.output_file, f"<Vertex> {i} {{ {vec.x} {vec.y} {vec.z}\n", self.indent)
+            self.indent += INDENT_AMOUNT
+            _write_with_indent(self.output_file, f"<UV> {{ {vertex.u} {vertex.v} }}\n", self.indent)
+            self.indent -= INDENT_AMOUNT
+            _write_with_indent(self.output_file, "}\n", self.indent)
+        self.indent -= INDENT_AMOUNT
+        _write_with_indent(self.output_file, "}\n", self.indent)
+
+        # TODO: How do these vertices connect together...?
+        # NOTE: These are probably always triangle strips...
+        r, g, b, a = self.vertex_rgba
+        for i in range(0, len(prim.vertices)-2):
+            _write_with_indent(self.output_file, f"<Polygon> {{\n", self.indent)
+            self.indent += INDENT_AMOUNT
+            if self.texture_id != -1:
+                _write_with_indent(self.output_file, f"<TRef> {{ {self.texture_id} }}\n", self.indent)
+            _write_with_indent(self.output_file, f"<RGBA> {{ {r} {g} {b} {a} }}\n", self.indent)
+            _write_with_indent(self.output_file, f"<BFace> {{ 0 }}\n", self.indent)
+            _write_with_indent(self.output_file, f"<VertexRef> {{ {i+0} {i+1} {i+2} <Ref> {{ {pool_name} }} }}\n", self.indent)
+            self.indent -= INDENT_AMOUNT
+            _write_with_indent(self.output_file, "}\n", self.indent)
+
+        self.__num_vertex_pools += 1
--- util.py
+++ util.py
@@ -7,6 +7,39 @@ class Vec4:
     z: float = 0.0
     w: float = 1.0
 
+    def __iter__(self):
+        return _VecIter(self)
+    
+    def __len__(self):
+        return 4
+    
+    def __getitem__(self, key: int) -> float:
+        match key:
+            case 0:
+                return self.x
+            case 1:
+                return self.y
+            case 2:
+                return self.z
+            case 3:
+                return self.w
+            case _:
+                raise IndexError()
+
+class _VecIter:
+    def __init__(self, vec: Vec4) -> None:
+        self.__vec = vec
+        self.__num = 0
+    
+    def __next__(self) -> float:
+        if self.__num >= len(self.__vec):
+            raise StopIteration
+        value = self.__vec[self.__num]
+        self.__num += 1
+        return value
+
+
+@dataclass
 class Color4(Vec4):
 
     @property
